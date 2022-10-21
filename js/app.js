037c61cd2c4a4728434a878185c87b95ff9001bf
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2022-10-21 23:59:54 +0330

    add To locaStorage fnc added part1

--- js/app.js
+++ js/app.js
@@ -7,6 +7,12 @@ const year = $.querySelector("#year");
 const thead = $.querySelector("thead");
 let bookArray = [];
 // functions
+// if localStorage is empty(add books array)
+function getLocalStorage() {
+  if (!localStorage.getItem("books")) {
+    localStorage.setItem("books", JSON.stringify(bookArray));
+  }
+}
 function numberValidator(e) {
   if (e.which < 48 || e.which > 57) {
     e.preventDefault();
@@ -25,7 +31,9 @@ function newBook(e) {
       bookAuthor: authorFiled.value,
       publishYear: year.value,
     };
+    bookArray.push(newBook);
     bookGenerator(newBook);
+    addToLocalStorage();
   }
 }
 // rendering in dom
@@ -44,13 +52,15 @@ function bookGenerator(bookObj) {
   //   appending new book to dom
   thead.append(trElm);
 }
-// if localStorage is empty(add books array)
-function getLocalStorage() {
-  if (!localStorage.getItem("books")) {
-    localStorage.setItem("books", JSON.stringify(bookArray));
-  }
-}
 
+function addToLocalStorage() {
+  //parse existing array
+  let ls = JSON.parse(localStorage.getItem("books"));
+  //   pushing new book to parsed array
+  ls.push(bookArray);
+  // push updated array to localstorage
+  let pushToLs = localStorage.setItem("books", JSON.stringify(bookArray));
+}
 
 // event listeners
 window.addEventListener("load", getLocalStorage);
