9009d858b607fe4c5a70ecbb471e3a017161f951
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2022-10-21 20:52:15 +0330

    variables and main comments added

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// variables
+const $=document;
+const titleField=$.querySelector('#title')
+const authorFiled=$.querySelector('#author')
+const year=$.querySelector('#year')
+
+// functions
+
+
+
+// event listeners
